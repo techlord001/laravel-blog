22e38cc4cd31272b2ec26b2d847c1a9b5d3224e5
Tech Lord <45688942+techlord001@users.noreply.github.com>
2023-10-07 13:01:31 +0100

    Add: Post Seeder

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,9 @@
 {{-- @extends('layouts.app') --}}
 
-@section('content')
+{{-- @section('content') --}}
     @foreach($posts as $post)
         <h2>{{ $post->title }}</h2>
         <p>{{ $post->content }}</p>
         <img src="{{ $post->image_path }}" alt="Post Image">
     @endforeach
-@endsection
+{{-- @endsection --}}
