dde05514f7a4d83dc9dfe63aa35dfa8b3b21feaf
Tech Lord <45688942+techlord001@users.noreply.github.com>
2023-10-07 11:17:06 +0100

    Add: Post Model, Migration, Factory & Update Testing Suite

--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -2,15 +2,31 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
 
 class PostTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic unit test example.
      */
     public function test_example(): void
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * Test if a post can be created.
+     * @test
+     */
+    public function it_can_create_a_post(): void
+    {
+        $post = Post::factory()->create();
+
+        $this->assertDatabaseHas('posts', ['title' => $post->title]);
     }
 }
